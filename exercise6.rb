b03e3ed73ba0c34fdef43af0cea4c5a2fa0c4947
Iva Delic <ms.iva.delic@gmail.com>
2015-04-01 14:51:24 -0400

    List of items with asterisk

--- exercise6.rb
+++ exercise6.rb
@@ -1,1 +1,4 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"] #this is an array
+grocery_list.each do |item|
+	puts "* #{item}"
+end
