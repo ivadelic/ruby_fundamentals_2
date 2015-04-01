18dfb9cc220dd3652c78b0f047341306637da433
Iva Delic <ms.iva.delic@gmail.com>
2015-04-01 16:48:49 -0400

    Deleted the second cohort

--- exercise7.rb
+++ exercise7.rb
@@ -13,11 +13,17 @@ end
 students[:cohort4] = 43
 cohorts(students)
 
-puts students.keys
+# puts students.keys
 
-students.each do |key, value|
-	new_value = (value*1.05).to_i
-	puts new_value
+def cohort_add(new_students)	
+	new_students.each do |key, value|
+		new_value = (value*1.05).to_i
+		new_students[key]=new_value 
+	end
 end
 
+cohort_add(students)
+cohorts(students)
 
+students.delete(:cohort2)
+cohorts(students)
